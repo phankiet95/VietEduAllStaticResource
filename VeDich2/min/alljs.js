1a7526e5bc744b277aad584109b956cb3fa469d4
Kiet Phan <kietphan54k2@gmail.com>
2024-11-12 18:19:41 +0900

    update 2

--- VeDich2/min/alljs.js
+++ VeDich2/min/alljs.js
@@ -520,18 +520,31 @@ $(document).on('click', '#exportHtml', function () {
   
   $(document).on('click', '#exportDataFile', function () {
     console.log('Start download Data');
-    //window.isLoading(true);
+    window.isLoading(true);
     // Lưu game Intro
     saveIntro();
     let dataSaved = JSON.stringify({ data: window.questionList, setting });
-    console.log(dataSaved);
-    download(`${window.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
+    //download(`${window.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
   
-    // setTimeout(() => {
-    //   window.isLoading(false);
-    // }, 1000)
+    downloadPlainText(`${window.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
+  
+    setTimeout(() => {
+      window.isLoading(false);
+    }, 1000)
   
   });
+
+  function downloadPlainText(filename, text) {
+    console.log('New Download function');
+    const blob = new Blob([text], { type: 'text/plain' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
+  }
   
   $(document).on('change', '#importHtml', function () {
     try {
